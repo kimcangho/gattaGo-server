d0421a4a33c4a02c6688774162c0c9a28ea78f30
Kent Ho <ho.kimcang@gmail.com>
2023-05-25 15:52:18 -0400

    initialize server with typescript, .env and nodemon packages

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express, { Express, Request, Response } from "express";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const app: Express = express();
+const PORT: string = process.env.PORT! || "8888";
+
+app.get("/", (_req: Request, res: Response) => {
+  res.send(`Paddles up! gattaGo is taking it away on PORT ${PORT}! 🚣🏼`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Paddles up! gattaGo is taking it away on PORT ${PORT}! 🚣🏼`);
+});
