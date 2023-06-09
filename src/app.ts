3d60222830d6539ac5a69797acd598deb9f6e532
Kent Ho <ho.kimcang@gmail.com>
2023-06-09 19:07:23 -0400

    revise server setup order and import cors/cookieParser libraries

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
 import express, { Express, Request, Response } from "express";
-import * as dotenv from "dotenv";
-dotenv.config();
-
-const app: Express = express();
-const PORT: string = process.env.PORT! || "8888";
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
+import dotenv from "dotenv";
 import regatta from "./api/routes/regatta.route";
 import team from "./api/routes/team.route";
 import athlete from "./api/routes/athlete.route";
+
+dotenv.config();
+const app: Express = express();
+const PORT: string = process.env.PORT! || "8888";
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/regattas", regatta);
 app.use("/teams", team);
--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,7 @@
 import express, { Express } from "express";
-import * as dotenv from "dotenv";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 // import loginRouter from "./routes/loginRoute";
 // import tokenRouter from "./routes/tokenRoute";
 // import logoutRouter from "./routes/logoutRoute";
@@ -8,10 +10,9 @@ dotenv.config();
 const app: Express = express();
 const PORT: number | string = process.env.AUTH_PORT || 7777;
 
-//  Middleware
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
-// app.use(cors());
-// app.use(cookieParser())
+app.use(cookieParser());
 
 //  Routes
 // app.use("/login", loginRouter);     //  creates access and refresh tokens
