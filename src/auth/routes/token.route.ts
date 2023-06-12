7db87346d7bc0b975eb7bfc16c0234c20c475570
Kent Ho <ho.kimcang@gmail.com>
2023-06-11 23:56:17 -0400

    include token route logic to validate refresh token and issue new access token

--- src/auth/routes/token.route.ts
+++ src/auth/routes/token.route.ts
@@ -1,19 +1,40 @@
 import { Router, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
+const { authRefreshToken } = new PrismaClient();
 
 const tokenRouter: Router = Router();
 
-tokenRouter.route("/").post((req: Request, res: Response) => {
-  //  Logic
+tokenRouter.route("/").post(async (req: Request, res: Response) => {
+  const { email, refreshToken } = req.body;
+  if (!refreshToken) return res.status(404).send("No refresh token found!");
 
-  tokenRouter.route("/").post((req: Request, res: Response) => {
-    //  get refresh token from header
-    //  check for refresh token in DB
-    //  verify refresh token through jwt
-    //  generate access token and return access token
+  const foundHashedToken = await authRefreshToken.findUnique({
+    where: {
+      email,
+    },
   });
+  if (!foundHashedToken)
+    return res.status(404).send("No refresh token found in DB!");
 
-  console.log("Token route");
-  res.status(200).send();
+  const comparedTokens = await bcrypt.compare(
+    refreshToken,
+    foundHashedToken!.id
+  );
+  if (!comparedTokens) return res.status(404).send("Tokens do not match!");
+
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET!, () => {
+    const accessToken: string | jwt.JwtPayload = jwt.sign(
+      { email },
+      process.env.ACCESS_TOKEN_SECRET!,
+      {
+        expiresIn: "30s",
+      }
+    );
+    res.status(200).send({ accessToken });
+  });
 });
 
 export default tokenRouter;
