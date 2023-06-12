00728592392752d7d432874b808f301858e97f42
Kent Ho <ho.kimcang@gmail.com>
2023-06-12 12:41:34 -0400

    add home route for auth server

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,4 @@
-import express, { Express } from "express";
+import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
@@ -12,13 +12,20 @@ const app: Express = express();
 const PORT: number | string = process.env.AUTH_PORT || 7777;
 
 app.use(cors({ origin: "http://localhost:5173" }));
+// app.use(cors({ origin: "*" }));
+
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/register", registerRouter);
 app.use("/login", loginRouter);
 app.use("/logout", logoutRouter);
 app.use("/token", tokenRouter);
+
+app.get("/", (_req: Request, res: Response) => {
+  res.send(`Paddles up! gattaGo is taking it away on PORT ${PORT}! 🚣🏼`);
+});
 
 app.listen(PORT, (): void => {
   console.log(`gattaGo auth server marshalling on PORT ${PORT}! 🪪`);
