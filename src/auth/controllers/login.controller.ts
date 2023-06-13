9a22f822ff14841d60a9023a9f6394200f79965a
Kent Ho <ho.kimcang@gmail.com>
2023-06-13 14:43:17 -0400

    refactor services out of login controller

--- src/auth/controllers/login.controller.ts
+++ src/auth/controllers/login.controller.ts
@@ -1,28 +1,22 @@
 import { Request, Response } from "express";
 import { compareHash, hashEntity } from "../utils/bcrypt.utils";
 import { generateToken } from "../utils/jwt.utils";
-import { PrismaClient } from "@prisma/client";
-const { user, authRefreshToken } = new PrismaClient();
+import {
+  findUser,
+  deleteRefreshToken,
+  addRefreshToken,
+  findPassword,
+} from "../services/login.services";
 
 const loginUser = async (req: Request, res: Response) => {
   const { email, password } = req.body;
-
   if (!email || !password) return res.status(400).send("Invalid request!");
 
-  const foundEmail = await user.findUnique({
-    where: {
-      email,
-    },
-  });
+  const foundEmail = await findUser(email);
   if (foundEmail!.email !== email) return res.status(404).send("Error!");
 
-  const foundHashedPassword = await user.findUnique({
-    where: {
-      email,
-    },
-  });
-
-  if (!(await compareHash(password, foundHashedPassword!.password)))
+  const foundHashedPassword = await findPassword(email);
+  if (!(await compareHash(password, foundHashedPassword)))
     return res.status(401).send("Password does not match!");
 
   const accessToken = await generateToken(
@@ -36,20 +30,9 @@ const loginUser = async (req: Request, res: Response) => {
     process.env.REFRESH_TOKEN_SECRET!
   );
 
-  await authRefreshToken.deleteMany({
-    where: {
-      email,
-    },
-  });
-
+  await deleteRefreshToken(email);
   const hashedRefreshToken = await hashEntity(refreshToken);
-
-  await authRefreshToken.create({
-    data: {
-      id: hashedRefreshToken,
-      email,
-    },
-  });
+  await addRefreshToken(hashedRefreshToken, email);
 
   res.cookie("accessToken", accessToken, {
     maxAge: 600000,
--- src/auth/services/login.services.ts
+++ src/auth/services/login.services.ts
@@ -1,4 +1,38 @@
 import { PrismaClient } from "@prisma/client";
 const { user, authRefreshToken } = new PrismaClient();
 
-export {};
+const findUser = async (email: string) => {
+  return await user.findUnique({
+    where: {
+      email,
+    },
+  });
+};
+
+const findPassword = async (email: string) => {
+  const foundUser = await user.findUnique({
+    where: {
+      email,
+    },
+  });
+  return foundUser!.password;
+};
+
+const deleteRefreshToken = async (email: string) => {
+  await authRefreshToken.deleteMany({
+    where: {
+      email,
+    },
+  });
+};
+
+const addRefreshToken = async (id: string, email: string) => {
+  await authRefreshToken.create({
+    data: {
+      id,
+      email,
+    },
+  });
+};
+
+export { findUser, findPassword, deleteRefreshToken, addRefreshToken };
