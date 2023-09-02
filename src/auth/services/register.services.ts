4ddb4036c8a40a6314cf5f202d5a35a918b479ab
Kent Ho <ho.kimcang@gmail.com>
2023-09-02 14:07:23 -0400

    clean up index.ts and register services

--- src/auth/services/register.services.ts
+++ src/auth/services/register.services.ts
@@ -1,14 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 const { user } = new PrismaClient();
 
-//  Review API call to this route and add return statement
 const findUser = async (email: string) => {
   const foundEmail = await user.findUnique({
     where: {
       email,
     },
   });
-  if (!foundEmail) throw new Error();
+  if (foundEmail) throw new Error();
 };
 
 const createUser = async (email: string, password: string) => {
