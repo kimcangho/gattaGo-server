97b47c96b2488a6a68112e4626b3200360c675d8
Kent Ho <ho.kimcang@gmail.com>
2023-10-01 12:28:31 -0400

    comment out credentials

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ app.use(
   cors({
     // origin: ["http://localhost:5173", "https://gatta-go-client.vercel.app/"],
     origin: "*",
-    credentials: true,
+    // credentials: true,
   })
 );
 
